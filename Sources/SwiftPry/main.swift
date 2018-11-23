763187e35ab0b39c9f400695a207ea8a34903ce7
bannzai <kingkong999yhirose@gmail.com>
2018-11-23 18:37:50 +0900

    :construction: wip save commit

--- Sources/SwiftPry/main.swift
+++ Sources/SwiftPry/main.swift
@@ -1,5 +1,13 @@
 import SwiftPryCore
-SwiftBulid().exec { (binaryPath) in
-    print(binaryPath)
+import Foundation
+
+//SwiftBulid().exec { (binaryPath) in
+//    print(binaryPath)
+//}
+//LLDBBridge(binaryPath: "").launch()
+var hoge: String? = readLine()!
+while readLine() == nil {
+    hoge = readLine()
 }
+print("hoge: \(String(describing: hoge))")
 print("Hello, world!")
--- Sources/SwiftPryCore/Command/LLDB/LLDBBridge.swift
+++ Sources/SwiftPryCore/Command/LLDB/LLDBBridge.swift
@@ -1,8 +1,25 @@
+import Foundation
 import SwiftShell
 
 
 public struct LLDBBridge {
-    public init() {
-        
+    let binaryPath: String
+    public init(binaryPath: String) {
+        self.binaryPath = binaryPath
+    }
+    
+    public func launch() {
+        let command = main.runAsync(bash: "lldb")
+        command.stdout.onStringOutput { (text) in
+            print(text)
+        }
+        command.stderror.onStringOutput { (text) in
+            print(text)
+        }
+        do {
+            try command.finish()
+        }  catch {
+            print(error.localizedDescription)
+        }
     }
 }
